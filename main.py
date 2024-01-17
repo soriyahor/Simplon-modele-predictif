205ac4156399cc29c8042e75c7ffdc1860b750f8
soriyahor <hor.soriya@gmail.com>
2024-01-17 12:19:11 +0100

    changement de parametre pour V2

--- main.py
+++ main.py
@@ -21,14 +21,14 @@ def predict(model, longitude, latitude):
     input_data = [[longitude, latitude]]
     return model.predict(input_data)[0]
 
-def predict_two(model, n_pieces, surface_habitable, prix_m2, code_postal, longitude, latitude):
+def predict_two(model, n_pieces, surface_habitable, code_postal, longitude, latitude):
     if model is None:
         raise HTTPException(status_code=503, detail="Modèle non chargé")
-    input_data = [[n_pieces, surface_habitable, prix_m2, code_postal, longitude, latitude]]
+    input_data = [[n_pieces, surface_habitable, code_postal, longitude, latitude]]
     return model.predict(input_data)[0]
 
 # Route pour effectuer la prédiction
-@app.post('/prix_m2', description="Permet de donner une prédiction du prix au mètre carré en fonction de la longitude et la latitude sur Paris pour des 4pièces")
+@app.post('/prix_m2', description="Permet de donner une prédiction du prix au mètre carré en fonction de la longitude et la latitude sur Paris pour des 4pièces en 2022")
 def prix_m2(longitude: float, latitude: float):
     # Chargement du modèle à chaque appel
     loaded_model = load_model('optimal_rfr_model.pkl')
@@ -41,7 +41,7 @@ def prix_m2(longitude: float, latitude: float):
     prediction = predict(loaded_model, longitude, latitude)
     return {'predicted_prix_m2': prediction}
 
-@app.post('/prix_m2_two', description="Permet de donner une prédiction du prix au mètre carré en fonction du nombre de pièce, de la surface habitable, du code postal, de la longitude et la latitude pour tout bien confondu")
+@app.post('/prix_m2_two', description="Permet de donner une prédiction du prix au mètre carré en fonction du nombre de pièce, de la surface habitable, du code postal, de la longitude et la latitude pour tout bien confondu en 2022")
 def prix_m2_two(n_pieces: int, surface_habitable: float, code_postal: int, longitude: float, latitude: float):
     # Chargement du modèle à chaque appel
     loaded_model = load_model('optimal_rfr_model_idf.pkl')
