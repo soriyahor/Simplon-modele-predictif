6d049df1c3ef8f18467c93b892425affd49ac202
soriyahor <hor.soriya@gmail.com>
2024-01-17 11:37:57 +0100

    ajout de modele idf

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def predict_two(model, n_pieces, surface_habitable, prix_m2, code_postal, longit
     return model.predict(input_data)[0]
 
 # Route pour effectuer la prédiction
-@app.post('/prix_m2', description="Permet de donner une prédiction du prix au mètre carré en fonction de longitude et la latitude")
+@app.post('/prix_m2', description="Permet de donner une prédiction du prix au mètre carré en fonction de la longitude et la latitude sur Paris pour des 4pièces")
 def prix_m2(longitude: float, latitude: float):
     # Chargement du modèle à chaque appel
     loaded_model = load_model('optimal_rfr_model.pkl')
@@ -41,17 +41,17 @@ def prix_m2(longitude: float, latitude: float):
     prediction = predict(loaded_model, longitude, latitude)
     return {'predicted_prix_m2': prediction}
 
-@app.post('/prix_m2_two', description="Permet de donner une prédiction du prix au mètre carré en fonction du nombre de pièce, de la surface habitable, du code postal, de la longitude et la latitude")
-def prix_m2_two(n_pieces: int, surface_habitable: float, prix_m2: float, code_postal: int, longitude: float, latitude: float):
+@app.post('/prix_m2_two', description="Permet de donner une prédiction du prix au mètre carré en fonction du nombre de pièce, de la surface habitable, du code postal, de la longitude et la latitude pour tout bien confondu")
+def prix_m2_two(n_pieces: int, surface_habitable: float, code_postal: int, longitude: float, latitude: float):
     # Chargement du modèle à chaque appel
-    loaded_model = load_model('optimal_linear_regression_model.pkl')
+    loaded_model = load_model('optimal_rfr_model_idf.pkl')
     
     # Vérification du chargement du modèle
     if loaded_model is None:
         raise HTTPException(status_code=503, detail="Modèle non chargé")
 
     # Prédiction
-    prediction = predict_two(loaded_model, n_pieces, surface_habitable, prix_m2, code_postal, longitude, latitude)
+    prediction = predict_two(loaded_model, n_pieces, surface_habitable, code_postal, longitude, latitude)
     
     return {'predicted_prix_m2': prediction}
 
